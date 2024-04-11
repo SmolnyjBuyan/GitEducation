6f8f9991d13fd6543cfd89b16fd4a6e1496aa8a3
SmolnyjBuyan <564kot9918@mail.ru>
2024-04-11 14:34:32 +0300

    Python/Seminar/seminar_07/

---
+++ Python/Seminar/seminar_07/task_03.py
@@ -0,0 +1,28 @@
+# Напишите функцию same_by(characteristic, objects), которая
+# проверяет, все ли объекты имеют одинаковое значение
+# некоторой характеристики, и возвращают True, если это так.
+# Если значение характеристики для разных объектов
+# отличается - то False. Для пустого набора объектов, функция
+# должна возвращать True. Аргумент characteristic - это
+# функция, которая принимает объект и вычисляет его
+# характеристику.
+
+# Ввод: 
+# values = [0, 2, 10, 6]
+# if same_by(lambda x: x % 2, values):
+# print(‘same’)
+# else:
+# print(‘different’)
+      
+# Вывод:
+# values = [0, 2, 10, 6] same
+
+def same_by(characteristic, objects):
+    return len(set(map(characteristic, objects))) in [0, 1]
+
+
+values = [15, 9, 18, 21]
+if same_by(lambda x: x % 3, values):
+    print('same')
+else:
+    print('different')
