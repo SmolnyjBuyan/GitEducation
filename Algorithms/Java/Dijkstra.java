99170bffd4a3b9ce4a21fc234babb37abde9ffee
SmolnyjBuyan <564kot9918@mail.ru>
2024-05-14 20:57:08 +0300

    Finished Dijkstra algorithm

--- Algorithms/Java/Dijkstra.java
+++ Algorithms/Java/Dijkstra.java
@@ -23,14 +23,6 @@ public class Dijkstra {
 
         graph.put("fin", new HashMap<>());
 
-        System.out.println(graph.get("start").get("a"));
-        System.out.println(graph.get("start").get("b"));
-        System.out.println(graph.get("a").get("fin"));
-        System.out.println(graph.get("start"));
-        System.out.println(graph.get("fin").keySet());
-        System.out.println(graph);
-        System.out.println(processed);
-
         costs.put("a", 6);
         costs.put("b", 2);
         costs.put("fin", infinity);
@@ -38,15 +30,53 @@ public class Dijkstra {
         parents.put("a", "start");
         parents.put("b", "start");
         parents.put("fin", null);
-        
-        
 
+        System.out.println("graph: " + graph);
+        System.out.println("costs: " + costs);
+        System.out.println("parents: " + parents);
+        System.out.println("processed: " + processed );
+   
+        String node = findLowestCostNode(costs, processed);
 
+        while (node != null) {
+            Integer cost = costs.get(node);
+            Map<String, Integer> neighbours = graph.get(node);
 
+            for (Map.Entry<String, Integer> entry : neighbours.entrySet()) {
+                String key = entry.getKey();
+                Integer value = entry.getValue();
 
+                Integer newCost = cost + value;
+                if (costs.get(key) > newCost) {
+                    costs.put(key, newCost);
+                    parents.put(key, node);
+                }
+            }
+            processed.add(node);
+
+            System.out.println("graph: " + graph);
+            System.out.println("costs: " + costs);
+            System.out.println("parents: " + parents);
+            System.out.println("processed: " + processed );
+
+            node = findLowestCostNode(costs, processed);
+        }
     }
 
-    // public static String findLowestCostNode(Map<String,Integer> nodes) {
-    //     return node;
-    // }
+    public static String findLowestCostNode(Map<String, Integer> costs, ArrayList<String> processed) {
+        Integer lowestCost = Integer.MAX_VALUE;
+        String lowestCostNode = null;
+
+        for (Map.Entry<String, Integer> entry : costs.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+
+            if (value < lowestCost && !processed.contains(key)) {
+                lowestCost = value;
+                lowestCostNode = key;
+            } 
+        }
+
+        return lowestCostNode;
+    }
 }
