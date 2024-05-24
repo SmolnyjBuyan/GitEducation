d5df44fc33adea2bcca2eeca85d4aa8b5372e6e0
SmolnyjBuyan <564kot9918@mail.ru>
2024-05-24 16:40:31 +0300

    Finish Java/Homework/homework_05/Task_03

--- Java/Homework/homework_05/Task_03.java
+++ Java/Homework/homework_05/Task_03.java
@@ -4,39 +4,45 @@ public class Task_03 {
     public static void main(String[] args) {
         int[] array = new int[]{10, 3, 76, 34, 23, 32};
 
-        // HeapSort.buildTree(array, 0);
-        HeapSort.heapSort(array, 0);
+        HeapSort.heapSort(array);
         System.out.println(Arrays.toString(array));
     }
 }
 
 class HeapSort {
-    public static void buildTree(int[] tree, int sortLength) {
-        int largest = sortLength;
-        int l = 2 * sortLength + 1;
-        int r = 2 * sortLength + 2;
 
-        if (l < tree.length && tree[l] > tree[largest]) {
-            largest = l;
+    public static void buildTree(int[] array, int index, int sortLength) {
+        int root = index;
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+
+        if (left < sortLength && array[left] > array[root]) {
+            root = left;
         }
 
-        if (r < tree.length && tree[r] > tree[largest]) {
-            largest = r;
+        if (right < sortLength && array[right] > array[root]) {
+            root = right;
         }
 
-        if  (largest != sortLength) {
-            int temp = tree[largest];
-            tree[largest] = tree[sortLength];
-            tree[sortLength] = temp;
-
-            buildTree(tree, largest);
+        if (root != index) {
+            int temp = array[root];
+            array[root] = array[index];
+            array[index] = temp;
+            buildTree(array, root, sortLength);
         }
-
     }
 
-    public static void heapSort(int[] sortArray, int sortLength) {
+    public static void heapSort(int[] sortArray) {
         for (int i = sortArray.length / 2 - 1; i >= 0; i--) {
-            buildTree(sortArray, sortLength);
+            buildTree(sortArray, i, sortArray.length);
+        }
+
+        for (int i = sortArray.length - 1; i >= 0; i--) {
+            int temp = sortArray[0];
+            sortArray[0] = sortArray[i];
+            sortArray[i] = temp;
+
+            buildTree(sortArray, 0, i);
         }
     }
 }
