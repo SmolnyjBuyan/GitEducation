ec241c67d5ba6969b2e17cfde4288f781a244082
SmolnyjBuyan <564kot9918@mail.ru>
2024-10-28 10:52:29 +0300

    add philosofyJava

---
+++ Java/Filosofia_Java/filosofiaJava/src/URShift.java
@@ -0,0 +1,11 @@
+public class URShift {
+    public static void main(String[] args) {
+        char с = 58;
+
+        System.out.println(с);
+
+        for (int i = 1, j = i + 10; i < 5; i++, j = j * 2) {
+        }
+
+    }
+}
