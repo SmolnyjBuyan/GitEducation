feea88f0d75a8b15234ebceb29a5719c3a7faa83
SmolnyjBuyan <564kot9918@mail.ru>
2025-02-16 23:53:36 +0300

    modify the SettingsWindow 1.1

--- JavaDevelopmentKit/Lection/lection_01/src/main/java/SettingsWindow.java
+++ JavaDevelopmentKit/Lection/lection_01/src/main/java/SettingsWindow.java
@@ -5,28 +5,39 @@ public class SettingsWindow extends JFrame {
     public static final int HEIGHT = 200;
     public static final int WIDTH = 300;
 
-    JButton btn3x3 = new JButton("3x3");
-    JButton btn5x5 = new JButton("5x5");
+//    JButton btn3x3 = new JButton("3x3");
+//    JButton btn5x5 = new JButton("5x5");
+    JRadioButton btn1P = new JRadioButton("1 PLAYER", true);
+    JRadioButton btn2P = new JRadioButton("2 PLAYERS");
+
     SettingsWindow(GameWindow gameWindow) {
-        setLayout(new GridLayout(3,1));
+        setLayout(new GridLayout(4,1));
         setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(gameWindow);
-        JLabel message = new JLabel("CHOOSE THE FIELD SIZE");
+
+        JLabel message = new JLabel("CHOOSE YOUR GAME MODE");
         message.setFont(new Font("Arial", Font.BOLD, 20));
         message.setHorizontalAlignment(SwingConstants.CENTER);
-        btn3x3.setFont(new Font("Arial", Font.BOLD, 20));
-        btn5x5.setFont(new Font("Arial", Font.BOLD, 20));
+        btn1P.setFont(new Font("Arial", Font.BOLD, 15));
+        btn2P.setFont(new Font("Arial", Font.BOLD, 15));
+        ButtonGroup gameModeButtonsGroup = new ButtonGroup();
+        gameModeButtonsGroup.add(btn1P);
+        gameModeButtonsGroup.add(btn2P);
+        JPanel gameModeButtonsPanel = new JPanel(new GridLayout(1, 2));
+        gameModeButtonsPanel.add(btn1P);
+        gameModeButtonsPanel.add(btn2P);
+        btn1P.setHorizontalAlignment(SwingConstants.CENTER);
+        btn2P.setHorizontalAlignment(SwingConstants.CENTER);
         add(message);
-        add(btn3x3);
-        add(btn5x5);
-        btn3x3.addActionListener(e -> {
-            gameWindow.startNewGame(0, 3,3, 3);
-            setVisible(false);
-        });
-        btn5x5.addActionListener(e -> {
-            gameWindow.startNewGame(0, 5,5, 4);
-            setVisible(false);
-        });
+        add(gameModeButtonsPanel);
+//        btn3x3.addActionListener(e -> {
+//            gameWindow.startNewGame(0, 3,3, 3);
+//            setVisible(false);
+//        });
+//        btn5x5.addActionListener(e -> {
+//            gameWindow.startNewGame(0, 5,5, 4);
+//            setVisible(false);
+//        });
     }
 
 
