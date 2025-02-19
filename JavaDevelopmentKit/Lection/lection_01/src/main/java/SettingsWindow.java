5b7d97c88de2e5e9ce7357ff2872c394b3cf2e46
SmolnyjBuyan <564kot9918@mail.ru>
2025-02-19 18:04:50 +0300

    modify the SettingsWindow 1.5

--- JavaDevelopmentKit/Lection/lection_01/src/main/java/SettingsWindow.java
+++ JavaDevelopmentKit/Lection/lection_01/src/main/java/SettingsWindow.java
@@ -1,9 +1,5 @@
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class SettingsWindow extends JFrame {
     private static final int HEIGHT = 400;
@@ -11,28 +7,54 @@ public class SettingsWindow extends JFrame {
     private static final int MIN_FIELD_SIZE = 3;
     private static final int MAX_FIELD_SIZE = 10;
 
+    private final GameWindow gameWindow;
+    private StylizedPanel gameModePanel;
+    private StylizedPanel winLengthPanel;
+    private StylizedPanel fieldSizePanel;
+    private StylizedButton startButton;
+    private StylizedSlider winLengthSlider;
+    private StylizedSlider fieldSizeSlider;
+
 
     SettingsWindow(GameWindow gameWindow) {
+        this.gameWindow = gameWindow;
+
         setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));
         setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(gameWindow);
 
+        initGameModeWidget();
+        initFieldSizeWidget();
+        initWinLengthWidget();
+        initStartButton();
+
+        add(gameModePanel);
+        add(fieldSizePanel);
+        add(winLengthPanel);
+        add(startButton);
+    }
+
+    private void initGameModeWidget() {
         StylizedRadioButton btn1P = new StylizedRadioButton("1 PLAYER", true);
         StylizedRadioButton btn2P = new StylizedRadioButton("2 PLAYERS");
         ButtonGroup gameModeButtonGroup = new ButtonGroup();
         gameModeButtonGroup.add(btn1P);
         gameModeButtonGroup.add(btn2P);
-        StylizedPanel gameModeButtonPanel = new StylizedPanel(new GridLayout(1, 2), "GAME MODE");
-        gameModeButtonPanel.add(btn1P);
-        gameModeButtonPanel.add(btn2P);
+        gameModePanel = new StylizedPanel(new FlowLayout(), "GAME MODE");
+        gameModePanel.add(btn1P);
+        gameModePanel.add(btn2P);
+    }
 
-        StylizedPanel winLengthPanel = new StylizedPanel(new GridLayout(1, 1), "WIN LENGTH");
-        StylizedSlider winLengthSlider = new StylizedSlider(MIN_FIELD_SIZE, MAX_FIELD_SIZE, MIN_FIELD_SIZE);
+    private void initWinLengthWidget() {
+        winLengthPanel = new StylizedPanel(new GridLayout(1, 1), "WIN LENGTH");
+        winLengthSlider = new StylizedSlider(MIN_FIELD_SIZE, MAX_FIELD_SIZE, MIN_FIELD_SIZE);
         winLengthSlider.setExtent(MAX_FIELD_SIZE - MIN_FIELD_SIZE);
         winLengthPanel.add(winLengthSlider);
+    }
 
-        StylizedPanel fieldSizePanel = new StylizedPanel(new GridLayout(2, 1), "FIELD SIZE");
-        StylizedSlider fieldSizeSlider = new StylizedSlider(MIN_FIELD_SIZE, MAX_FIELD_SIZE, MIN_FIELD_SIZE);
+    private void initFieldSizeWidget() {
+        fieldSizePanel = new StylizedPanel(new GridLayout(2, 1), "FIELD SIZE");
+        fieldSizeSlider = new StylizedSlider(MIN_FIELD_SIZE, MAX_FIELD_SIZE, MIN_FIELD_SIZE);
         StylizedLabel fieldSize = new StylizedLabel("3x3");
         fieldSizeSlider.addChangeListener
                 (e -> {
@@ -42,21 +64,17 @@ public class SettingsWindow extends JFrame {
                 });
         fieldSizePanel.add(fieldSize);
         fieldSizePanel.add(fieldSizeSlider);
-
-        StylizedButton startButton = new StylizedButton("START");
-        startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        startButton.addActionListener(e -> {
-            gameWindow.startNewGame(1, fieldSizeSlider.getValue(),
-                    fieldSizeSlider.getValue(), winLengthSlider.getValue());
-            setVisible(false);
-        });
-
-        add(gameModeButtonPanel);
-        add(fieldSizePanel);
-        add(winLengthPanel);
-        add(startButton);
     }
 
+    private void initStartButton() {
+        startButton = new StylizedButton("START");
+        startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        startButton.addActionListener(e -> startNewGame());
+    }
 
-
+    private void startNewGame() {
+        gameWindow.startNewGame(1, fieldSizeSlider.getValue(),
+                fieldSizeSlider.getValue(), winLengthSlider.getValue());
+        setVisible(false);
+    }
 }
--- JavaDevelopmentKit/Lection/lection_01/src/main/java/StylizedRadioButton.java
+++ JavaDevelopmentKit/Lection/lection_01/src/main/java/StylizedRadioButton.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class StylizedRadioButton extends JRadioButton {
     public StylizedRadioButton(String text) {
         super(text);
-        setFont(new Font("Arial", Font.BOLD, 15));
+        setFont(new Font("Arial", Font.BOLD, 17));
     }
 
     public StylizedRadioButton(String text, boolean selected) {
