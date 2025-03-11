cf28d473247c9ad2dfb8378993452e74d81ac9e9
SmolnyjBuyan <564kot9918@mail.ru>
2025-03-11 18:10:28 +0300

    JDK/seminar_04: current time is 56:52

---
+++ JavaDevelopmentKit/Seminar/seminar_04/src/main/java/org/example/Task03.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task03 {
+    public static void main(String[] args) {
+        Map<Long, String> dictionary = new HashMap<>();
+        dictionary.put(89116668866L, "Андрей");
+        dictionary.put(89115556644L, "Сергей");
+        dictionary.put(89119994433L, "Дмитрий");
+    }
+}
