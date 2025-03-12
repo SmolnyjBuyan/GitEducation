13398fc8ec2731ac99703aa4ab27f3cae8f8740b
SmolnyjBuyan <564kot9918@mail.ru>
2025-03-12 07:57:47 +0300

    complete the JDK/seminar_04

--- JavaDevelopmentKit/Seminar/seminar_04/src/main/java/org/example/Task03.java
+++ JavaDevelopmentKit/Seminar/seminar_04/src/main/java/org/example/Task03.java
@@ -1,13 +1,22 @@
 package org.example;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Task03 {
     public static void main(String[] args) {
         Map<Long, String> dictionary = new HashMap<>();
         dictionary.put(89116668866L, "Андрей");
         dictionary.put(89115556644L, "Сергей");
-        dictionary.put(89119994433L, "Дмитрий");
+        dictionary.put(41356L, "Дмитрий");
+        System.out.println(dictionary.get(dictionary.keySet().stream().min(Comparator.naturalOrder()).orElse(null)));
+
+        String maxName = dictionary.values().stream().max(Comparator.naturalOrder()).orElse(null);
+        System.out.println(dictionary.entrySet().stream().filter
+                (entry -> entry.getValue().equals(maxName)).collect(Collectors.toList()));
     }
+
+
 }
